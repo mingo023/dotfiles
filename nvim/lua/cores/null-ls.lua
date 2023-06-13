bede00f70ad308898a76f7b822354a447107de23
immint023 <nvminh023@gmail.com>
2023-06-13 12:51:33 +0700

    🚀 config null-ls

--- nvim/lua/cores/null-ls.lua
+++ nvim/lua/cores/null-ls.lua
@@ -1,6 +1,7 @@
 local null_ls = require("null-ls")
 local h = require("null-ls.helpers")
 local methods = require("null-ls.methods")
+local on_attach = require("lsp/on_attach")
 
 local FORMATTING = methods.internal.FORMATTING
 
@@ -14,5 +15,6 @@ null_ls.setup({
         null_ls.builtins.diagnostics.golangci_lint,
         null_ls.builtins.diagnostics.eslint_d,
     },
+    on_attach = on_attach,
 })
 
