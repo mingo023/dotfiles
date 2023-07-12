5cbda23df4a0d7539dc6498e6c4a48633e92faef
immint023 <nvminh023@gmail.com>
2023-07-12 07:30:33 +0700

    add mapping to resize windows

--- nvim/lua/mappings/personal.lua
+++ nvim/lua/mappings/personal.lua
@@ -5,8 +5,7 @@ map("i", "jj", "<ESC>", { silent = true })
 -- enable copy to clipboard
 map("v", "<C-c>", '"+y')
 
--- remove matchings
-map("n", "<Leader>dm", ":noh <CR>", { silent = true })
+-- remove matchings map("n", "<Leader>dm", ":noh <CR>", { silent = true })
 
 -- search all the words match with current selection
 map("v", "/", 'y/<C-R>"<CR>N', { silent = true })
@@ -30,3 +29,9 @@ map("v", ":", ":s/")
 
 -- remap :w to <cmd+s> (check at: ~/.config/alacritty/alacritty.yml:705)
 map("c", "w<CR>", 'echo "🚨 use <cmd+s> instead"<CR>')
+
+map("n", "<A-Left>", ":vertical resize +5<CR>")
+map("n", "<A-Right>", ":vertical resize -5<CR>")
+map("n", "<A-Up>", ":resize +5<CR>")
+map("n", "<A-Down>", ":resize -5<CR>")
+
