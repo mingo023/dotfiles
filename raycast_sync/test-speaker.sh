c4b57a6320b31cad61fc97271c381a318f718876
immint023 <nvminh023@gmail.com>
2023-07-20 23:33:49 +0700

    add speaker check

---
+++ raycast_sync/test-speaker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title test speaker
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🔊
+
+# Documentation:
+# @raycast.author immint023
+# @raycast.authorURL https://raycast.com/immint023
+
+say -v "Samantha" "speaker is working fine"
+
