d612d42a5d2a5f0ec4e967d440b4f622fdbaa83b
immint023 <nvminh023@gmail.com>
2023-09-17 19:20:44 +0700

    config cokeline

--- nvim/lua/cores/dashboard.lua
+++ nvim/lua/cores/dashboard.lua
@@ -69,17 +69,3 @@ theme.config.layout = {
 }
 
 alpha.setup(theme.config)
-
--- autocommands to turn off bars
-local alpha_group = vim.api.nvim_create_augroup("alpha", {clear = true})
-vim.api.nvim_create_autocmd("User", {
-  group = alpha_group,
-  pattern = "AlphaReady",
-  command = "set laststatus=0 | set showtabline=0"
-})
-
-vim.api.nvim_create_autocmd("User", {
-  group = alpha_group,
-  pattern = "AlphaClosed",
-  command = "set laststatus=3 | set showtabline=0"
-})
