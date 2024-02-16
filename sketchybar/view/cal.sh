325056996f877941021c218dc89aab00e60bcc00
immint023 <nvminh023@gmail.com>
2024-02-16 13:22:52 +0700

    update font update frequency for sketchybar cal

--- sketchybar/view/cal.sh
+++ sketchybar/view/cal.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-sketchybar --add item   cal right                                     \
-           --set cal    update_freq=180                               \
-                        script="$PLUGIN_DIR/cal.sh"                   \
+sketchybar --add item   cal right        \
+           --set cal    update_freq=10  \
+           script="$PLUGIN_DIR/cal.sh"   \
