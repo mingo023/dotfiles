96630dea247b44f7105bdc61479d2fae009e8dd8
immint023 <nvminh023@gmail.com>
2024-02-26 16:16:47 +0700

    update statusline

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -5,3 +5,11 @@ require("lsp/diagnostic")
 require("lsp/protobuf")
 require("lsp/ruby")
 require("lsp/tailwind")
+
+vim.fn.sign_define(
+  "DiagnosticSignError",
+  { text = "", texthl = "DiagnosticSignError", numhl = "DiagnosticSignError" }
+)
+vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint", numhl = "DiagnosticSignHint" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo", numhl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn", numhl = "DiagnosticSignWarn" })
