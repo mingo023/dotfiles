941f2c5b57d49568c60a0c21110807f935137976
immint023 <nvminh023@gmail.com>
2024-05-02 14:11:02 +0700

    optimize

--- nvim/lua/cores/treesitter.lua
+++ nvim/lua/cores/treesitter.lua
@@ -26,5 +26,12 @@ require("nvim-treesitter.configs").setup({
   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
   highlight = {
     enable = true,
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
   },
 })
--- nvim/lua/packages/utils.lua
+++ nvim/lua/packages/utils.lua
@@ -1,6 +1,11 @@
 return {
   {
     "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 500
+    end,
     config = function()
       require("which-key").setup({})
     end,
