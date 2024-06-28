08c6573d5809b3fe74a828dbf8b3e80394ea459b
immint023 <nvminh023@gmail.com>
2024-06-28 20:23:37 +0700

    config yabai

--- nvim/lua/mappings/movement.lua
+++ nvim/lua/mappings/movement.lua
@@ -5,3 +5,7 @@ local harpoon = require("harpoon")
 map("n", "<Leader>ha", function()
   harpoon:list():add()
 end, { noremap = true, silent = true })
+
+map("n", "<Leader>hm", function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end, { noremap = true, silent = true })
--- nvim/lua/mappings/telescope.lua
+++ nvim/lua/mappings/telescope.lua
@@ -108,28 +108,3 @@ function display_nestjs_file_structure_types_menu()
 end
 
 map("n", ",p", display_nestjs_file_structure_types_menu, { silent = true })
-
-local harpoon = require("harpoon")
-local conf = require("telescope.config").values
-
-local function toggle_telescope(harpoon_files)
-  local file_paths = {}
-  for _, item in ipairs(harpoon_files.items) do
-    table.insert(file_paths, item.value)
-  end
-
-  require("telescope.pickers")
-    .new({}, {
-      prompt_title = "Harpoon",
-      finder = require("telescope.finders").new_table({
-        results = file_paths,
-      }),
-      previewer = conf.file_previewer({}),
-      sorter = conf.generic_sorter({}),
-    })
-    :find()
-end
-
-vim.keymap.set("n", "<Leader>hm", function()
-  toggle_telescope(harpoon:list())
-end, { desc = "Open harpoon window" })
