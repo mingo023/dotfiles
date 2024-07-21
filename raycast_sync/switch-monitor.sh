26b2704728339bc253e17a5c8cd58ad841303bd5
immint023 <nvminh023@gmail.com>
2024-07-21 16:22:38 +0700

    add raycast script command

---
+++ raycast_sync/switch-monitor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Switch monitor
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author immint023
+# @raycast.authorURL https://raycast.com/immint023
+
+~/.config/oss/m1ddc/m1ddc display BF70314E-776B-4ACE-AA02-C1D2E319CA4B set input 15
+
