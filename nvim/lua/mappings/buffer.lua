dd8732d16193362703c569a90b118796191e1650
immint023 <nvminh023@gmail.com>
2024-09-03 11:30:24 +0700

    setup buffer move for corne

--- nvim/lua/mappings/buffer.lua
+++ nvim/lua/mappings/buffer.lua
@@ -19,3 +19,24 @@ for i = 1,9 do
     silent = true,
   })
 end
+
+map("n", "<Leader>a", "<Cmd>BufferLineGoToBuffer 1<CR>", {
+  noremap = true,
+  silent = true,
+})
+map("n", "<Leader>s", "<Cmd>BufferLineGoToBuffer 2<CR>", {
+  noremap = true,
+  silent = true,
+})
+map("n", "<Leader>d", "<Cmd>BufferLineGoToBuffer 3<CR>", {
+  noremap = true,
+  silent = true,
+})
+map("n", "<Leader>f", "<Cmd>BufferLineGoToBuffer 4<CR>", {
+  noremap = true,
+  silent = true,
+})
+map("n", "<Leader>g", "<Cmd>BufferLinegoToBuffer 5<CR>", {
+  noremap = true,
+  silent = true,
+})
