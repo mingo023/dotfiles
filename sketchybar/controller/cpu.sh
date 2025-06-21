67df6f30b816005169f80a1b93740c7ae3a7954a
Minh Ngo <nvminh023@gmail.com>
2025-06-21 16:23:10 +0700

    yo

--- sketchybar/controller/cpu.sh
+++ sketchybar/controller/cpu.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
-CPU=$(top -l  2 | grep -E "^CPU" | tail -1 | awk '{ print $3 + $5 }')
+CPU_USAGE=$(top -l  2 | grep -E "^CPU" | tail -1 | awk '{ print $3 + $5 }')
 
 CPUICON=􀫥
 
 MAGENTA=#ff80ff
-sketchybar -m --set $NAME icon=" $CPUICON" label="$CPU%" label.color=0xff${MAGENTA:1} icon.color=0xff${MAGENTA:1}
+
+sketchybar -m --set $NAME icon=$CPUICON label="$CPU_USAGE%" label.color=0xff${MAGENTA:1} icon.color=0xff${MAGENTA:1}
