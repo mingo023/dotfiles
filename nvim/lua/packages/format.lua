e573f629e2cebdeb0e24af92047e8806ba70f905
Minh Ngo <nvminh023@gmail.com>
2025-07-27 23:51:34 +0700

    yo

--- nvim/lua/packages/format.lua
+++ nvim/lua/packages/format.lua
@@ -13,12 +13,6 @@ return {
     end,
   },
   {
-    "folke/ts-comments.nvim",
-    opts = {},
-    event = "VeryLazy",
-    enabled = vim.fn.has("nvim-0.10.0") == 1,
-  },
-  {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
